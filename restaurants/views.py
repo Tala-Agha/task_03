3fa0db2b0e36d704e4399dbd256464824a95f94f
Tala Agha <macbook@MacBooks-MacBook-Pro.local>
2020-02-12 15:02:56 +0200

    Rest list

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,14 +6,20 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
-    }
+        "my_list":[
+            {"restaurant_name":"pizza alrais","food_type":"Flavors of Italy & Lebanon"},
+            {"restaurant_name":"Taqueria","food_type":"Mexican"},
+            {"restaurant_name":"Pow Burger","food_type":"Burgers"},]}
     return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
 
     context = {
+    "my_object": {
+    "restaurant_name":"pow burger",
+    "food_type":"burgers"
+    }
 
     }
     return render(request, 'detail.html', context)
